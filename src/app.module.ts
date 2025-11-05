59312e51c6b3c6eb199d391cd5a0bba81f1a2405
kdw8219 <kdw8219@gmail.com>
2025-11-05 23:45:29 +0900

    Feature  - add initial setting(add robot resource, repository injection set etc)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RobotModule } from './robot/robot.module';
 
 @Module({
-  imports: [],
+  imports: [RobotModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/robot/robot.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { RobotService } from './robot.service';
+import { RobotController } from './robot.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Robot } from './entities/robot.entity'
+
+@Module({
+  imports:[TypeOrmModule.forFeature([Robot])],
+  controllers: [RobotController],
+  providers: [RobotService],
+})
+export class RobotModule {}
