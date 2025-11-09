a17f8d227fb2996425ffd96b2f1670173b766877
kdw8219 <kdw8219@gmail.com>
2025-11-09 15:51:36 +0900

    Feature  - fix docker error  - add winston logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { RobotModule } from './robot/robot.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { ComutilService } from './comutil/comutil.service';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
@@ -22,6 +24,28 @@ import { ComutilService } from './comutil/comutil.service';
       }),
       inject: [/* ConfigService */ ConfigService],
     }),
+
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.timestamp(),
+            winston.format.printf(({ timestamp, level, message }) => {
+              return `[${timestamp}] ${level}: ${message}`;
+            }),
+          ),
+        }),
+        new winston.transports.File({
+          filename: 'logs/app.log',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+        }),
+      ],
+    }),
+
     RobotModule,
     
    ],
